7116e0c0da4d05a559c84e10b4dc7d30932bf03e
mkg33 <maja.k.gwozdz@gmail.com>
2018-04-07 17:07:08 +0200

    password.java
    A very simple brute-force password cracker.

---
+++ password.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Console;
+import java.util.*;
+
+public class password {
+	
+	public static void main(String[] args) {
+	
+	// Console console = System.console();
+		
+	Scanner user_inp = new Scanner(System.in);
+	
+	String all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 ";
+	
+	//doesn't work with other characters
+	
+	char[] alphabet = all.toCharArray();
+	
+	String result = "";
+	
+	// char[] password = console.readPassword("Password: ");
+	
+	System.out.println("A very simple brute-force password cracker. Enter an alphanumeric password with four characters.\n\n");
+	
+	System.out.print("Enter your password: ");
+	
+	String password = user_inp.nextLine();
+	
+	// String password1 = password.toString();
+	
+	if(password.length() >= 5 || password.length()<4) {
+		
+		System.out.println("Password too short or too long. Exiting.");
+		
+	}
+	
+	else {
+	
+	outerloop:
+	for(int i=0; i<alphabet.length; i++) {
+		
+		
+		for(int j=0; j<alphabet.length; j++) {
+			
+			result = ""+ alphabet[i] + alphabet[j];
+			
+			for(int k=0; k<alphabet.length; k++) {
+				
+				result = "" + alphabet[i] + alphabet[j] + alphabet[k];
+				
+				for(int l=0; l<alphabet.length; l++) {
+					
+					result = "" + alphabet[i] + alphabet[j] + alphabet[k] + alphabet[l];
+					
+						
+						
+						
+						if(result.equals(password)) {
+							
+							System.out.println("Found it! Your password is: " + result);
+							break outerloop;
+							
+						}
+						
+						else {
+							System.out.println(result);
+						}
+						
+						
+					
+					
+				}
+				
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	
+	}
+
+}
+
+}
